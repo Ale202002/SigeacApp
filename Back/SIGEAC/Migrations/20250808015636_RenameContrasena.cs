accb1518344e5f0d3b31090e9e1eaf1d2dbb4237
GonzaZ2 <gonzalozordan@gmail.com>
2025-08-07 23:02:20 -0300

    se borraron las ñ

---
+++ Back/SIGEAC/Migrations/20250808015636_RenameContrasena.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SIGEAC.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameContrasena : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Contraseña",
+                table: "Usuarios",
+                newName: "Contrasena");
+        }
+
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Contrasena",
+                table: "Usuarios",
+                newName: "Contraseña");
+        }
+    }
+}
