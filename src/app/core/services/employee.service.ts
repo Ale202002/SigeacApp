0e72fc565389ba0765c8181c2bbee681fcd2c9d8
Matias Juri <matias.play.juri@gmail.com>
2025-08-15 01:44:43 -0300

    Agregado de enums y servicios

---
+++ src/app/core/services/employee.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Employee, EmployeeCreate, EmployeeUpdate } from '../models/Employee.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+  private readonly API_URL = '';
+
+  constructor(private http: HttpClient) {}
+
+  crear(employee: EmployeeCreate): Observable<any> {
+    return this.http.post(${this.API_URL}/crear, employee);
+  }
+
+  modificar(id: number, employee: EmployeeUpdate): Observable<any> {
+    return this.http.put(${this.API_URL}/modificar/${id}, employee);
+  }
+
+  listar(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(${this.API_URL}/listar);
+  }
+
+  buscar(id: number): Observable<Employee> {
+    return this.http.get<Employee>(${this.API_URL}/buscar/${id});
+  }
+
+  eliminar(id: number): Observable<any> {
+    return this.http.delete(${this.API_URL}/eliminar/${id});
+  }
+
+  // Método específico para buscar empleado por usuario
+  buscarPorUsuario(usuarioId: number): Observable<Employee> {
+    return this.http.get<Employee>(${this.API_URL}/buscar-por-usuario/${usuarioId});
+  }
+
+  // Validar si DNI ya existe
+  validarDNI(dni: string, employeeId?: number): Observable<boolean> {
+    const params = employeeId ? ?employeeId=${employeeId} : '';
+    return this.http.get<boolean>(${this.API_URL}/validar-dni/${dni}${params});
+  }
+
+  // Validar si correo ya existe
+  validarCorreo(correo: string, employeeId?: number): Observable<boolean> {
+    const params = employeeId ? `?employeeId=${employeeId}` : '';
+    return this.http.get<boolean>(`${this.API_URL}/validar-correo/${correo}${params}`);
+  }
+}
