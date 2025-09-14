84acbeec7480780a4b9fb43d1bc47f8e950c643f
Thomas <thomgut15@gmail.com>
2025-09-14 13:35:21 -0300

    Se adapto toda la estructura del proyecto para la utilizacion de: por un lado, "enums" y por el otro, "services", donde tenemos implementations y interfaces donde se establecio la logica de negocios, todo esto respetando la logica que se venia utilizando en controllers. + se actualizo ContextoDB por error en todos los endpoints

--- Back/SIGEAC/Data/ContextoDB.cs
+++ Back/SIGEAC/Data/ContextoDB.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SIGEAC.Enums;
 using SIGEAC.Models;
 
 namespace SIGEAC.Data
---
+++ Back/SIGEAC/Migrations/20250914161914_CrearTablasComponentesPerifericosEquipos.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SIGEAC.Migrations
+{
+    /// <inheritdoc />
+    public partial class CrearTablasComponentesPerifericosEquipos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Componentes_Equipos_EquipoID",
+                table: "Componentes");
+
+            migrationBuilder.CreateTable(
+                name: "Perifericos",
+                columns: table => new
+                {
+                    ID_Periferico = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Tipo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Estado = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EquipoID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Perifericos", x => x.ID_Periferico);
+                    table.ForeignKey(
+                        name: "FK_Perifericos_Equipos_EquipoID",
+                        column: x => x.EquipoID,
+                        principalTable: "Equipos",
+                        principalColumn: "ID_Equipo",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Perifericos_EquipoID",
+                table: "Perifericos",
+                column: "EquipoID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Componentes_Equipos_EquipoID",
+                table: "Componentes",
+                column: "EquipoID",
+                principalTable: "Equipos",
+                principalColumn: "ID_Equipo",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Componentes_Equipos_EquipoID",
+                table: "Componentes");
+
+            migrationBuilder.DropTable(
+                name: "Perifericos");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Componentes_Equipos_EquipoID",
+                table: "Componentes",
+                column: "EquipoID",
+                principalTable: "Equipos",
+                principalColumn: "ID_Equipo");
+        }
+    }
+}
